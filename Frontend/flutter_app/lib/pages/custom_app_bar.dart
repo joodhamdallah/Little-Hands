ed695220b04fd4d4f4ad7555c883bbd9c89a81d0
Jood Hamdallah <jood.hamdallah12@gmail.com>
2025-05-05 01:43:28 +0300

    Made custom app bar and bottom nav and used them in parent and caregiver homepage

---
+++ Frontend/flutter_app/lib/pages/custom_app_bar.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final bool showIcons;
+  final List<Widget>? customActions;
+  final String? title;
+
+  const CustomAppBar({
+    super.key,
+    this.showIcons = true,
+    this.customActions,
+    this.title,
+  });
+
+  @override
+  Size get preferredSize => const Size.fromHeight(60);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: const Color(0xFFFF600A),
+      elevation: 0.5,
+      centerTitle: true,
+      title:
+          title != null
+              ? Text(
+                title!,
+                style: const TextStyle(
+                  fontFamily: 'NotoSansArabic',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              )
+              : null,
+      leading: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Image.asset('assets/images/logo_without_bg.png'),
+      ),
+      actions:
+          customActions ??
+          (showIcons
+              ? [
+                IconButton(icon: const Icon(Icons.search), onPressed: () {}),
+                IconButton(
+                  icon: const Icon(Icons.notifications),
+                  onPressed: () {},
+                ),
+              ]
+              : []),
+    );
+  }
+}
