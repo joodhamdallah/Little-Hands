f0447127e8ac032ce1b4954e69a7ac0fbc38c23c
Saleh Sawalha <sawalha.saleh3@gmail.com>
2025-05-16 00:37:50 +0300

    do some fixes on specialNeedsService to add the rating and training_certifications

--- Backend/services/specialNeedsService.js
+++ Backend/services/specialNeedsService.js
@@ -5,6 +5,25 @@ class SpecialNeedsService {
     const exists = await SpecialNeeds.findOne({ user_id: userId });
     if (exists) throw new Error("Profile already exists");
 
+
+    if (data.rate && data.rate_type) {
+      data.rate = {
+        amount: parseFloat(data.rate),
+        type: data.rate_type
+      };
+      delete data.rate_type;
+    }
+
+    if (data.training_certifications) {
+      data.trainings = data.training_certifications;
+      delete data.training_certifications;
+    }
+
+    if (data.can_accompany_to_school !== undefined) {
+      data.can_accompany_in_school = data.can_accompany_to_school;
+      delete data.can_accompany_to_school;
+    }
+
     const newProfile = new SpecialNeeds({
       user_id: userId,
       ...data
