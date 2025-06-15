19a4928a085430b828d281e2e5ed2a133c04f64b
Saleh Sawalha <sawalha.saleh3@gmail.com>
2025-06-15 23:05:50 +0300

    starting with web by flutter

---
+++ Frontend/flutter_app/web/firebase-messaging-sw.js
@@ -0,0 +1,17 @@
+// web/firebase-messaging-sw.js
+
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
+
+// TODO: Replace with your own config
+firebase.initializeApp({
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
+});
+
+// Retrieve messaging instance
+const messaging = firebase.messaging();
