c21267a0ca7deb2ed7bca78715cbfa884efcf030
Jood Hamdallah <jood.hamdallah12@gmail.com>
2025-06-21 07:02:00 +0300

    Created feedbacks page for caregiver, adjusted some other stuff

--- Frontend/flutter_app/lib/Caregiver/Home/CaregiverFeedbacksPage.dart
+++ Frontend/flutter_app/lib/Caregiver/Home/CaregiverFeedbacksPage.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/pages/config.dart';
 import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class CaregiverFeedbacksPage extends StatefulWidget {
